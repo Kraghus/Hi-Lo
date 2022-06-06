99dd152e60cfb61e056a0227645ca58f41e0f882
Kraghus <masongage@outlook.com>
2022-06-06 10:37:51 -0400

    Update main.cpp
    Added game loop so the user can continue to play without restarting the program.

--- main.cpp
+++ main.cpp
@@ -9,36 +9,64 @@ std::uniform_int_distribution<> randomRoll{ 1, 100 }; // using integers 1 throug
 
 int main()
 {
-	std::cout << "**********************************************************************************\n";
-	std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n\n";
-
-	// call a mersenne twister and store it as an integer
-	int randomNumber{ (randomRoll(mt)) };
-
-	// user gets 7 tries to guess the number
-	for (int guesses{ 1 }; (guesses <= 7); ++guesses)
+	while (true)
 	{
-		std::cout << "Guess #" << guesses << ": ";
-		int userNumber{};
-		std::cin >> userNumber;
-		{
+		std::cout << "*********************************************************************************\n";
+		std::cout << "Let's play Hi-Lo! I'm thinking of a number. You have 7 tries to guess what it is.\n\n";
 
-			if (userNumber == randomNumber)
+		// call a mersenne twister and store it as an integer
+		int randomNumber{ (randomRoll(mt)) };
+
+		// user gets 7 tries to guess the number
+		for (int guesses{ 1 }; (guesses <= 7); ++guesses)
+		{
+			std::cout << "Guess #" << guesses << ": ";
+			int userNumber{};
+			std::cin >> userNumber;
 			{
-				std::cout << "Correct! You win!\n\n";
-				return 0;
-			}
-			else if (userNumber > randomNumber)
-			{
-				std::cout << "Your guess is too high.\n\n";
-			}
-			else if (userNumber < randomNumber)
-			{
-				std::cout << "Your guess is too low.\n\n";
+
+				if (userNumber == randomNumber)
+				{
+					std::cout << "Correct! You win!\n\n";
+					return 0;
+				}
+				else if (userNumber > randomNumber)
+				{
+					std::cout << "Your guess is too high.\n\n";
+				}
+				else if (userNumber < randomNumber)
+				{
+					std::cout << "Your guess is too low.\n\n";
+				}
 			}
 		}
+		std::cout << "...........................................\n";
+		std::cout << "Sorry, you lose. The correct number was " << randomNumber << ".\n";
+		std::cout << "...........................................\n\n";
+
+		while (true)
+		{
+			std::cout << "Would you like to play again? Enter 'Y' for YES or 'N' for NO.\n";
+			char yesOrNo{};
+			std::cin >> yesOrNo;
+
+		
+			if ((yesOrNo == 'y') || (yesOrNo == 'Y'))
+			{
+				break;
+			}
+			else if ((yesOrNo == 'n') || (yesOrNo == 'N'))
+			{
+				std::cout << "Thank you for playing Hi-Lo!\n";
+				return 0;
+			}
+			else
+			{
+				std::cout << "Invalid input detected. Please try again!\n";
+				continue;
+			}
+		}
+
+
 	}
-	std::cout << "...........................................\n";
-	std::cout << "Sorry, you lose. The correct number was " << randomNumber << ".\n";
-	return 0;
 }
