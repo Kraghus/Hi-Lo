ec273effd65d64040fbde16af7d37ed76783254b
Kraghus <masongage@outlook.com>
2022-06-06 10:54:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main()
 			int userNumber{};
 			std::cin >> userNumber;
 
-			// clear any extraneous input
+			// clears any extraneous input
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 			// tests and fixes failed extractions and overflow
