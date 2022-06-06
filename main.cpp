1f4f1814ac548ff7656bfcb8a12d0114a85937b0
Kraghus <masongage@outlook.com>
2022-06-06 10:24:25 -0400

    Update main.cpp
    Tweaked documentation and output presentation.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <random>
 
+// mersenne twister
+static std::random_device rd; // call numbers from OS
+static std::seed_seq seedSequence{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() }; // seed with OS numbers
+static std::mt19937 mt{ seedSequence }; // assign random number with seed
+std::uniform_int_distribution<> randomRoll{ 1, 100 }; // using integers 1 through 100
+
 int main()
 {
-	// call random numbers from the OS and store them in rd
-	std::random_device rd;
-	// get 8 integers of random numbers from std::random_device for our seed
-	std::seed_seq seedSequence{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() }; 
-	// initialize our Mersenne Twister with the std::seed_seq
-	std::mt19937 mt{ seedSequence }; // initialize our Mersenne Twister with the std::seed_seq
+	std::cout << "**********************************************************************************\n";
+	std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n\n";
 
-	// Create a reusable random number generator that generates uniform numbers between 1 and 100
-	std::uniform_int_distribution<> randomRoll{ 1, 100 };
-
-	std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
-
-	
+	// call a mersenne twister and store it as an integer
 	int randomNumber{ (randomRoll(mt)) };
+
+	// user gets 7 tries to guess the number
 	for (int guesses{ 1 }; (guesses <= 7); ++guesses)
 	{
 		std::cout << "Guess #" << guesses << ": ";
@@ -26,19 +25,20 @@ int main()
 
 			if (userNumber == randomNumber)
 			{
-				std::cout << "Correct! You win!\n";
+				std::cout << "Correct! You win!\n\n";
 				return 0;
 			}
 			else if (userNumber > randomNumber)
 			{
-				std::cout << "Your guess is too high.\n";
+				std::cout << "Your guess is too high.\n\n";
 			}
-			else
+			else if (userNumber < randomNumber)
 			{
-				std::cout << "Your guess is too low.\n";
+				std::cout << "Your guess is too low.\n\n";
 			}
 		}
 	}
+	std::cout << "...........................................\n";
 	std::cout << "Sorry, you lose. The correct number was " << randomNumber << ".\n";
 	return 0;
 }
