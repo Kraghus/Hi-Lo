643f1f8903b696d08638702d96aabddb1e1811d5
Kraghus <masongage@outlook.com>
2022-06-09 15:12:14 -0400

    Bug fix
    Fixed extraneous input bleeding into guess input after asking the user if they want to play again.

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main()
 				std::cout << "...........................................\n\n";
 			}
 		}
-		
+
 
 
 		while (true)
@@ -62,6 +62,15 @@ int main()
 			char yesOrNo{};
 			std::cin >> yesOrNo;
 
+			// clears any extraneous input
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+			// tests and fixes failed extractions and overflow
+			if (!std::cin)
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
 
 			if ((yesOrNo == 'y') || (yesOrNo == 'Y'))
 			{
